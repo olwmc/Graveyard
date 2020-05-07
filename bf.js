28aaecc1805130f4bc13a691a3ad0e5d618af4fc
Oliver Wendell McLaughlin <36379339+olwmc@users.noreply.github.com>
2020-05-07 14:51:26 -0400

    Create bf.js

---
+++ bf.js
@@ -0,0 +1,1 @@
+g=x=>{a='',c=0,p=Array(99).fill(0),[...x].map(e=>a+='p[c]++;p[c]--||(p[c]=255);c++;c--;console.log(String.fromCharCode(p[c]));p[c]=prompt();while(p[c]){;}'.split`;`['+-><.,[]'.indexOf(e)]+';');eval(a)};
